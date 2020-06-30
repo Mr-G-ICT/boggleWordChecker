0ec11d7d158469693a23d2bf49ab82311636813c
Mr B Gristwood <bgristwood@outlook.com>
2020-06-30 10:33:51 +0100

    now works with a 2d Jagged array as the solution requires it. Needed a new function to work out how many columns. As the rows are static size, used a simple length function, this cna be moved if the function becomes more jagged.

--- boggleWordChecker/Program.cs
+++ boggleWordChecker/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Runtime.ConstrainedExecution;
 using System.Text.Json.Serialization;
 using System.Threading;
 
@@ -8,43 +9,52 @@ namespace boggleWordChecker
     {
         static void Main(string[] args)
         {
-            char[,] boggleGrid = { { "I", "L", "A", "W" } ,
-                                     { "B","N","G","E" },
-                                     { "I","U","A","O"},
-                                     { "A","S","R","L"} };
+            char[][] boggleGrid = {new[] {'E','A','R','A'},
+         new[] {'N','L','E','C'},
+          new[] {'I','A','I','S'},
+         new[] {'B','Y','O','R'} };
 
         Console.WriteLine("Hello World!");
-            Console.WriteLine(boggleWordChecker(boggleGrid, "ILNBIA"));
+            Console.WriteLine(boggleWordChecker(boggleGrid, "EARS"));
         }
 
-        public static bool boggleWordChecker(char[,] board, string word)
+   
+
+        public static bool boggleWordChecker(char[][] board, string word)
         {
             //split the string
             //find the first letter
             //check letters around it
             //if they match, move on one letter.
-            bool result = false;
+           
 
             char[] lettersToCheck = word.ToCharArray();
-            
-            for(int letterCount = 0; letterCount < lettersToCheck.Length -1; letterCount++)
+            int numColumns = getColumnLength(board, 0);
+            int numRows = board[0].Length;
+
+            Console.WriteLine(numColumns + "num columns");
+            Console.WriteLine(numRows + " num rows");
+
+            bool result = false;
+
+            for (int letterCount = 0; letterCount < lettersToCheck.Length -1; letterCount++)
             {
                 Console.WriteLine("letter is" + lettersToCheck[letterCount]);
 
                 //loop through the grip
-                for(int Row = 0; Row <  board.GetLength(0); Row++)
+                for(int Row = 0; Row <  numRows; Row++)
                 {
-                    for(int Column = 0; Column < board.GetLength(1); Column++)
+                    for(int Column = 1; Column < numColumns; Column++)
                     {
                         //if you have a match.
-                        if (lettersToCheck[letterCount] == board[Row, Column])
+                        if (lettersToCheck[letterCount] == board[Row][Column])
                         {
                             //erase the letter so you can't reuse it.
-                            Console.WriteLine("match:   " + board[Row, Column]);
-                            board[Row, Column] = '';
+                            Console.WriteLine("match:   " + board[Row][ Column]);
+                            board[Row][Column] = '*';
 
                             //need to check if the letters around it are a match
-                            result = checkNextLetter(Row, Column, board, lettersToCheck[letterCount + 1]);
+                            result = checkNextLetter(Row, Column, board, lettersToCheck[letterCount + 1], numRows, numColumns);
                             Console.WriteLine(result);
                             if(result == false)
                             { 
@@ -74,23 +84,37 @@ namespace boggleWordChecker
             return true;
         }  //end function
 
-
-        public static bool checkNextLetter(int Row, int Column, char[,] board, char nextLetter )
+        public static int getColumnLength(char[][] arrays, int columnNumber)
         {
+            int count = 0;
+            for (int i = 0; i < arrays.Length; i++)
+            {
+                if (arrays[1].Length > columnNumber)
+                {
+                    count++;
+                }
+            }
+            return count;
+        }
+
+
+        public static bool checkNextLetter(int Row, int Column, char[][] board, char nextLetter, int numRows, int numColumns )
+        {
+            // as this is a 2D JAgged array, i'm sending the num rows and columns as these are pre-calculated
 
             //working down first
             for (int nextRow = -1; nextRow < 2; nextRow++)
             {
                 for (int nextColumn = -1; nextColumn < 2; nextColumn++)
                 {
-                    if ((Row + nextRow < 0) || (Row + nextRow) > board.GetLength(0) || (Column + nextColumn < 0) || (Column + nextColumn) > board.GetLength(1))
+                    if ((Row + nextRow < 0) || (Row + nextRow) > numRows || (Column + nextColumn < 0) || (Column + nextColumn) > numColumns)
                     {
                         Console.WriteLine("skip it");
                     }
                     else 
                     {
 
-                        if (nextLetter == board[Row + nextRow, Column + nextColumn])
+                        if (nextLetter == board[Row + nextRow][ Column + nextColumn])
                         {
                             Console.WriteLine(Row + nextRow + " and  " + Column + nextColumn);
                             Console.WriteLine("match");
